43b2dc73d7561778ecb71c6121affdf164026383
ajshopov <alexanderjshopov@gmail.com>
2017-11-29 17:34:16 +1100

    array homework

---
+++ Alex/wk01/3-wed/arrays.js
@@ -0,0 +1,31 @@
+//1.
+
+var days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log(days_of_the_week);
+
+//2.
+
+var mover = days_of_the_week.pop();
+console.log(days_of_the_week);
+
+days_of_the_week.unshift(mover);
+console.log(days_of_the_week);
+
+//3.
+
+var weekdays = days_of_the_week.slice(1,6);
+console.log(weekdays);
+
+var weekends = [].concat(days_of_the_week.slice(0,1), days_of_the_week.slice(-1));
+console.log(weekends);
+weekends.reverse();
+console.log(weekends);
+
+//4. ~~~~~~~~~~~
+
+//5.
+
+weekdays.sort();
+console.log(weekdays);
+
+
