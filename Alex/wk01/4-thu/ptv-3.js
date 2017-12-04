2a500db488c80b6cf2465ab60ba8b95e70f00aed
ajshopov <alexanderjshopov@gmail.com>
2017-12-04 16:46:16 +1100

    yay

---
+++ Alex/wk01/4-thu/ptv-3.js
@@ -0,0 +1,96 @@
+
+var changeDirection = function(dep, arr, track){
+      track = track.slice(0).reverse();
+      dep = track.length - dep - 1;
+      arr = track.length - arr -1;
+
+      return [dep, arr, track];
+}
+
+var alamein = ['Flinders', 'Richmond', 'East_Rich', 'Burnley', 'Hawthorn', 'Glenferrie'];
+var glenWaverly = ['Flagstaff', 'Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'];
+var sandringham = ['Southern_X', 'Richmond', 'South_Yarra', 'Prahran', 'Windsor'];
+
+
+var origin = prompt('FROM:');
+var dest = prompt('TO:');
+
+
+//check which line for origin
+var beg = alamein.indexOf(origin);
+var line1 = alamein.slice(0);
+    if(beg === -1){
+      beg = glenWaverly.indexOf(origin);
+      line1 = glenWaverly.slice(0);
+    }
+    if(beg === -1){
+      beg = sandringham.indexOf(origin);
+      line1 = sandringham.slice(0);
+    }
+//console.log('beginning: ' + beg + ' trainline ' + line1);
+
+//find destination line
+var end = alamein.indexOf(dest);
+var line2 = alamein.slice(0);
+    if(end === -1){
+      end = glenWaverly.indexOf(dest);
+      line2 = glenWaverly.slice(0);
+    }
+    if(end === -1){
+      end = sandringham.indexOf(dest);
+      line2 = sandringham.slice(0);
+    }
+//console.log('ending: ' + end + ' trainline ' + line2);
+
+var checker1 = line1.join();
+var checker2 = line2.join();
+console.log(checker1);
+console.log(checker2);
+
+//check that lines are the same, if not, change var transfer to Richmond
+if(checker1 == checker2){
+    
+      if(beg > end){
+          var newDirection = changeDirection(beg, end, line1);
+          beg = newDirection[0];
+          end = newDirection[1];
+          line1 = newDirection[2];
+      }
+
+      //slice array to trip 
+      var trip = line1.slice(beg, end+1);
+      console.log(trip);
+
+} else {
+      //set transfer for Richmond station
+      var transfer1 = line1.indexOf('Richmond');
+      var transfer2 = line2.indexOf('Richmond');
+
+      if(beg > transfer1){
+          var newDirection = changeDirection(beg, transfer1, line1);
+          beg = newDirection[0];
+          transfer1 = newDirection[1];
+          line1 = newDirection[2];        
+      }
+
+      if(transfer2 > end){
+          var newDirection = changeDirection(transfer2, end, line2);
+          transfer2 = newDirection[0];
+          end = newDirection[1];
+          line2 = newDirection[2];
+      }
+
+      //slice both arrays for 2 trips and join
+      var tripA = line1.slice(beg, transfer1);
+      var tripB = line2.slice(transfer2, end+1);
+      var trip = tripA.concat(tripB);
+      console.log(trip);
+}
+
+//output results
+console.log('~~~TRIP PLANNER~~~')
+console.log('origin: '+ origin);
+console.log ('destination: ' + dest);
+console.log(trip.join(" --> "));
+console.log((trip.length - 1) + ' stops total');
+
---
+++ Alex/wk01/5-fri/age.js
@@ -0,0 +1,98 @@
+// var d = new Date();
+
+// var currentYear = d.getFullYear();
+// var currentMonth = d.getMonth();
+// var currentDate = d.getDate();
+
+
+// var birthYear = prompt('what year were you born? (yyyy)');
+// var birthMonth = prompt('what month were you born? (mm)');
+// var birthDate = prompt('what day were you born? (dd)');
+
+// console.log(currentYear);
+// console.log(currentMonth);
+// console.log(currentDate);
+
+// console.log(birthYear);
+// console.log(birthMonth);
+// console.log(birthDate);
+
+// console.log('you are ');
+
+//var age = prompt('how old are you?')
+
+// var days = age * 365;
+
+// if(age % Math.sqrt(age) === 0){
+//   console.log('age is a perfect square');
+// }
+
+// console.log(days);
+
+
+//~~~~~~~~~~~~~~~~~~~~
+
+//print every character in a string
+
+// var word = prompt("enter test word");
+// var loopCount = 0;
+
+// while(loopCount < word.length){
+//   console.log(word[loopCount]);
+//   loopCount++
+// }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//while practice
+
+var numb = -10
+while(numb < 20){
+  console.log(numb);
+  numb++;
+}
+
+var even = 10;
+while(even < 41){
+  console.log(even);
+  even+=2;
+}
+
+var odd = 301;
+
+while(odd <334){
+  console.log(odd);
+  odd+=2;
+}
+
+var fizzbuzz = 5;
+while(fizzbuzz < 51){
+  if(fizzbuzz % 5 === 0 && fizzbuzz % 3 === 0){
+    console.log(fizzbuzz);
+  }
+fizzbuzz++;
+}
+
+//for practice
+
+for(var num = -10; numb < 20; numb++){
+  console.log(numb);
+}
+
+for(var even = 10; even < 41; even+=2){
+  console.log(even);
+}
+
+for(var odd1 = 301; odd1 <334; odd1+=2){
+  console.log(odd1);
+}
+
+for (var fizzbuzz1 = 5; fizzbuzz1 < 51;fizzbuzz1++){
+  if(fizzbuzz1 % 5 === 0 && fizzbuzz1 % 3 === 0){
+    console.log(fizzbuzz1);
+  }
+}
+
+
+
+
