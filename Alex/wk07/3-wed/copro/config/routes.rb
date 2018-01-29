06d6650af859b704b2ff55b00748e52b1fdcd813
ajshopov <alexanderjshopov@gmail.com>
2018-01-29 16:40:25 +1100

    has add comments feature and new likes star with js count

--- Alex/wk07/3-wed/copro/config/routes.rb
+++ Alex/wk07/3-wed/copro/config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  resources :likes
-  resources :comments
   get '/', to: 'pages#home'
   get '/login', to: 'session#new'
   post '/session', to: 'session#create'
   delete '/session', to: 'session#destroy'
   
+  resources :likes
   resources :projects, only: [:new, :create, :show]
   resources :users
   resources :comments
