c5ba46ece875cc9ef67966ab061b7b77488e61e1
ajshopov <alexanderjshopov@gmail.com>
2018-02-12 17:24:07 +1100

    monday wk10 hw phone test

--- Alex/wk08/4-thu/pokedex_app/app/controllers/application_controller.rb
+++ Alex/wk08/4-thu/pokedex_app/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+# helpers only available inside views
+# place current_user logged_in? etc here to access everywhere
