41b1ad2aebaa019c6c5e020deb6994d08cf51298
ajshopov <alexanderjshopov@gmail.com>
2018-02-15 10:32:08 +1100

    finished queens.rb passed tests!!

--- Alex/wk10/2-tue/queens.rb
+++ Alex/wk10/2-tue/queens.rb
@@ -10,8 +10,6 @@ class Queens
     end
   end 
 
-  
-
   def white
     if !@positions.nil?
       @positions[:white]
@@ -30,31 +28,35 @@ class Queens
   end
 
   def attack?
+    if black[0] == white[0] || black[1] == white[1] || black.inject(0, :+) == white.inject(0, :+) || black[1] - white[1] == black[0] - white[0]
+      true else false end
   end
 
   def to_s
     board = ""
+    black_marker = black[0] * 16 + black[1] * 2 + 1
+    white_marker = white[0] * 16 + white[1] * 2 + 1
     i = 1
     while i<128
+      if i == black_marker
+        board << 'B'
+      elsif i == white_marker
+        board << 'W'
       # odd add zero
-      if i.odd?
-        board << '0'
+      elsif i.odd?
+        board << 'O'
       elsif i % 16 == 0
         board << "\n"
       else
         board << ' '
       end
       # even add space
-
-
-
       i += 1
     end
     return board
   end
 
-
 end
 
-binding.pry
-puts 'end'
+# binding.pry
+# puts 'end'
--- Alex/wk10/3-wed/js-data-types/js-datatypes.js
+++ Alex/wk10/3-wed/js-data-types/js-datatypes.js
@@ -73,3 +73,28 @@ console.log(99 * "luftbaloons");
 // Separates the characters of a string into an array. This one is a string method.
 // split()
 
+var numbers = [2, 4, 6, 8]
+numbers.pop()
+numbers.push(10)
+numbers.unshift(3)
+
+// guess [3, 2, 4, 6, 10]
+console.log(numbers)
+
+var morse = ["dot", "pause", "dot"]
+var moreMorse = morse.join(" dash ")
+console.log(moreMorse.split(" "));
+
+// guess ['dot', 'dash', 'pause', 'dash', 'dot']
+
+var bands = []
+var beatles = ["Paul", "John", "George", "Pete"]
+var stones = ["Brian", "Mick", "Keith", "Ronnie", "Charlie"]
+bands.push(beatles)
+bands.unshift(stones)
+bands[bands.length - 1].pop()
+bands[0].shift()
+bands[1][3] = "Ringo"
+
+// [["Mick", "Keith", "Ronnie", "Charlie"],["Paul", "John", "George", "Ringo"]]
+
