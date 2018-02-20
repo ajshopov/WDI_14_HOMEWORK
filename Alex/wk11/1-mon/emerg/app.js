4790a9d10acae0d446e2486dbdc913de5efca790
ajshopov <alexanderjshopov@gmail.com>
2018-02-20 15:40:43 +1100

    tues hw display centered with rand colour and name input

--- Alex/wk11/1-mon/emerg/app.js
+++ Alex/wk11/1-mon/emerg/app.js
@@ -5,24 +5,34 @@ var _ = require('lodash');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get('/', function(req, res){
-  compliments = [
+var compliments = [
   "Your instructors love you",
   "High five = ^5",
   "Shut up and take my money",
   "It's almost beer o'clock",
   "The Force is strong with you"
-  ]
-  colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+]
+var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+var randomComp = _.sample(compliments)
+var randomCol = _.sample(colors)
 
-  randomC = _.sample(compliments)
-
+app.get('/', function(req, res){
   res.render('home', {
-    randomC: randomC
+    randomComp: randomComp,
+    randomCol: randomCol
   });
 });
 
+app.get('/*/', function(req, res){
+  var person = req.path.slice(1);
+  res.render('name', {
+    person: person,
+    randomComp: randomComp,
+    randomCol: randomCol
+  });
+});
 
 app.listen(PORT, function(){
   console.log(`listening on port ${PORT}`);
